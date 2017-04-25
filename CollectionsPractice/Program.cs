b900599a8fbfb7038f00c1180de570dbd058a48e
JLyons009 <JLyons009@gmail.com>
2017-04-25 12:40:13 -0500

    Created instance of List<Student> and tested sorting the collection.

--- CollectionsPractice/Program.cs
+++ CollectionsPractice/Program.cs
@@ -10,6 +10,19 @@ namespace CollectionsPractice
     {
         static void Main(string[] args)
         {
+            List<Student> students = new List<Student>
+            {
+                new Student() {Name = "Tormund", GradeLevel = 3},
+                new Student() {Name = "Ygritte", GradeLevel = 3},
+                new Student() {Name = "Tormund", GradeLevel = 2},
+            };
+
+            students.Sort();
+
+            foreach (Student student in students)
+            {
+                Console.WriteLine($"{student.Name} is in {student.GradeLevel}");
+            }
         }
     }
 }
